dae8943267b333256e0c8f8710c5bb6cd0da163b
mohammedismail007 <mohammedismail9788@gmail.com>
2024-02-29 20:04:08 +0530

    React-project

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './Header'
+
+function Home() {
+  return (
+    <div><Header /></div>
+  )
+}
+
+export default Home
--- src/Product.jsx
+++ src/Product.jsx
@@ -23,13 +23,29 @@ function Product(props) {
             {props.productsDetail.description?.substring(0, 35) + "..."}
           </p>
           <div className="flex justify-between items-center mt-4">
-            <button
+            {/* <button
               className="bg-blue-500 hover:bg-blue-600
              text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
               Rating: {props.productsDetail.rating.rate} out of{" "}
               {props.productsDetail.rating.count} peoples
-            </button>
+            </button> */}
+            <div>
+  <div class="flex items-center space-x-2 text-base">
+    <h4 class="font-semibold text-slate-900">Ratings</h4>
+    <span class="rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700">{props.productsDetail.rating.rate}</span>
+  </div>
+  <div class="mt-3 flex -space-x-2 overflow-hidden">
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+  </div>
+  <div class="mt-3 text-sm font-medium">
+    <a href="#" class="text-blue-500">{props.productsDetail.rating.count} peoples</a>
+  </div>
+</div>
           </div>
         </div>
       </div>
